094b0706704a06cfbfebe6c04a83b791819defdb
Rob Adams <robert.andrew.adams@gmail.com>
2017-05-18 15:05:58 +0100

    Site Has Name

--- src/site.test.jsx
+++ src/site.test.jsx
@@ -9,12 +9,27 @@ import { expect } from 'chai';
 
 describe('Site', () => {
 	it('should render a div with "site" class', () => {
-		const site = 'Hammerwood';
 		const component = renderIntoDocument(
 			<Site />
 		);
 		const siteElement = findRenderedDOMComponentWithClass(component, 'site');
 
 		expect(siteElement).to.be.ok;
+	});
+
+	it('should render a div with the site text', () => {
+		const site = 'Hammerwood';
+		const component = renderIntoDocument(
+			<Site
+				 	site= {site}
+				/>
+		);
+
+		const siteElement = findRenderedDOMComponentWithClass(component, 'site');
+
+		const siteText = siteElement.textContent
+
+		expect(siteText).to.equal('Hammerwood');
+
 	});
 });

f7d1107ef7bc73107abbf53e01a4e016e0cbd9fa
Rob Adams <robert.andrew.adams@gmail.com>
2017-05-20 10:20:12 +0100

    maps v1

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
+import {Gmaps, Marker, InfoWindow} from 'react-gmaps';
+
+const coords = {
+  lat: 54.7115618,
+  lng: -6.0656706
+};
+
+const params = {v: '3.exp'};
+
+
 
 export class MapContainer extends Component{
+
+
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: false
+    });
+  }
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onClick(data) {
+    console.log('onClick', data);
+    console.log(self);
+  }
+
+  message(i){
+    console.log('message', i);
+
+  }
+
+
   render(){
+
+    const data = this.props.sites
+    const markers = data.map((data) =>
+       <Marker key={data.id}
+        lat={data.lat}
+        lng={data.lng}
+        label={"" + data.id}
+        onClick={this.message(data.name)}
+        />
+    );
     return (
       <div className='map'>
-
+        <Gmaps id='gmaps'
+        width={'600px'}
+        height={'400px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={5}
+        loadingMessage={'Be happy'}
+        params={params}
+        onMapCreated={this.onMapCreated}>
+        { markers }
+        </Gmaps>
       </div>
     );
   };

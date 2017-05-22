f4a7741b4b48935b6d30d7c1831ad1fc694b5e6e
Rob Adams <robert.andrew.adams@gmail.com>
2017-05-22 18:49:33 +0100

    Corrected App test

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -11,7 +11,11 @@ databaseURL:process.env.REACT_APP_FIREBASE_DATABASEURL,
 
 firebase.initializeApp(FIREBASE_OPTIONS);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
 });

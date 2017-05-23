ed1248da30fa23cd9d14ece7b5c016347ff36b81
Rob Adams <robert.andrew.adams@gmail.com>
2017-05-23 15:27:42 +0100

    Working Home link

--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -5,13 +5,8 @@ import {Home} from '../src/Home';
 describe('Home', () => {
 	it ('a has a link to homepage', () => {
 		const component = renderer.create(
-			<Home href="/">HOINK</Home>
+			<Home href="/">Home</Home>
     ).toJSON();
-
-		// const homeElement = findRenderedDOMComponentWithClass(component, 'Home');
-		// const url = homeElement.href
-		console.log(component);
 		expect(component).toMatchSnapshot();
-
 	});
 });
